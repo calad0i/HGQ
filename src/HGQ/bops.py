44356403376387afe7c6500ed2b4e0c1ed50b676
Chang Sun <chsun@cern.ch>
2023-08-11 17:26:59 +0200

    feat: cover_factor

--- src/HGQ/bops.py
+++ src/HGQ/bops.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 from .layers import HLayerBase
+from .utils import warn
 
 
 class FreeBOPs(tf.keras.callbacks.Callback):
@@ -52,7 +53,7 @@ class ExactBOPs(tf.keras.callbacks.Callback):
         logs['multi'] = bops
 
 
-def compute_bops(model, dataset, bsz=16384, verbose=True, return_results=False, rst=True):
+def compute_bops(model, dataset, bsz=16384, verbose=True, return_results=False, rst=True, cover_factor=1.0):
     if rst:
         for layer in model.layers:
             if isinstance(layer, HLayerBase):
@@ -68,6 +69,18 @@ def compute_bops(model, dataset, bsz=16384, verbose=True, return_results=False, 
         for i in range(0, dataset.shape[0], bsz):
             r.append(model(dataset[i:i + bsz], training=False))
 
+    if cover_factor!=1.0:
+        assert cover_factor > 0.
+        if cover_factor < 1:
+            warn(f'cover_factor<1.0 will likely to result in overflows.')
+        for layer in model.layers:
+            if not isinstance(layer, HLayerBase):
+                continue
+            aq = layer.pre_activation_quantizer
+            aq._min.assign(aq(aq._min*cover_factor)) # type: ignore
+            aq._max.assign(aq(aq._max*cover_factor)) # type: ignore
+            
+            
     bops = 0
     for layer in model.layers:
         if isinstance(layer, HLayerBase):
