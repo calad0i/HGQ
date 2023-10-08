7d3f706f021839cbfd03ced208e2688677c9ff48
Chang Sun <chsun@cern.ch>
2023-10-07 20:16:14 -0700

    fix another rounding issue

--- src/HGQ/layers/base.py
+++ src/HGQ/layers/base.py
@@ -203,11 +203,14 @@ class HLayerBase(tf.keras.layers.Layer):
         assert np.sum(kn[int_bits + fp_bits <= 0]
                       ) == 0, f'Bit counting error at {self.name}. Did you forget to call `compute_bops` before passing the model to converter? Or, please try again with cuda disabled (2^13 or above will may in error when tensorflow is run with cuda. If not, this should never happen. Please open an issue at https://github.com/calad0i/HGQ'
         
-        if np.prod(self.pre_activation_quantizer.fbw.shape) > 1: # ==1 means no mask should be applied
+        if np.prod(self.pre_activation_quantizer.fbw.shape) > 1:
+            # Is in io_parallel mode, 
             if self.pre_activation_quantizer.rnd_strategy != 3 and not self._has_bias:
+                # Using standard round and has no bias term to emulate the rounding
                 fp_max += 1
             return tuple_to_apf((kn_max, int_max, fp_max))
         else:
+            # No need to do 2-stage processing, just round the result if needed
             if self.pre_activation_quantizer.rnd_strategy != 3 and not self._has_bias:
                 rnd = 'AP_RND'
             else:
@@ -250,7 +253,16 @@ class HLayerBase(tf.keras.layers.Layer):
         int_max, fp_max, kn_max = int_bits[mask].max(), fp_bits[mask].max(), kn[mask].max()
         assert np.sum(
             kn[~mask]) == 0, f'Bit counting error at {self.name}. Did you forget to call `compute_bops` before passing the model to converter? Or, please try again with cuda disabled (2^13 or above will may in error when tensorflow is run with cuda. If not, this should never happen. Please open an issue at https://github.com/calad0i/HGQ'
-        return tuple_to_apf((kn_max, int_max, fp_max))
+        if np.prod(self.pre_activation_quantizer.fbw.shape) > 1: # ==1 means no mask should be applied
+            if self.pre_activation_quantizer.rnd_strategy != 3 and not self._has_bias:
+                fp_max += 1
+            return tuple_to_apf((kn_max, int_max, fp_max))
+        else:
+            if self.pre_activation_quantizer.rnd_strategy != 3 and not self._has_bias:
+                rnd = 'AP_RND'
+            else:
+                rnd = 'AP_TRN'
+            return tuple_to_apf((kn_max, int_max, fp_max), rnd=rnd)
 
     @property
     def ker_container(self):
