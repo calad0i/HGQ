c3fb162a2f97870d89de145378a6186de30cdb01
Chang Sun <chsun@cern.ch>
2023-10-29 23:31:45 -0700

    Add (new) preliminary tests

--- test/test_hgq_combined_huge.py
+++ test/test_hgq_combined_huge.py
@@ -21,7 +21,7 @@ try:
 except ImportError:
     pytest.skip("HGQ not installed, skipping tests", allow_module_level=True)
 
-from HGQ.layers import HQuantize, HDense, HConv2D, HConv1D, PMaxPool2D, PReshape, PMaxPool1D, PFlatten, HActivation
+from HGQ.layers import HQuantize, HDense, HConv2D, HConv1D, PMaxPool2D, PReshape, PMaxPool1D, PConcatenate, PFlatten, HActivation, HAdd, PDropout
 from HGQ import get_default_pre_activation_quantizer_config, set_default_pre_activation_quantizer_config
 from HGQ import trace_minmax
 from HGQ.proxy import generate_proxy_model
@@ -41,28 +41,41 @@ def create_model(rnd_strategy:str, io_type:str):
     pa_config['rnd_strategy'] = rnd_strategy
     set_default_pre_activation_quantizer_config(pa_config)
     
-    model = keras.Sequential([
-        HQuantize(input_shape=(10,10,1), name='inp_q'), # 10x10x1
-        HConv2D(2, 3, activation='relu'), # 8x8x2
-        PMaxPool2D(2), # 4x4x2
-        PReshape((8,4)), # 8x4
-        HConv1D(4, 3, activation='relu'), # 6x4
-        PMaxPool1D(2), # 3x4    2mismatch_proxy = r_keras != r_proxy
-        PFlatten(), # 12
-        HDense(10), # 10
-        HActivation('sigmoid')
-    ])
+    inp = keras.Input(shape=(10,10,))
+    qinp = HQuantize()(inp)
+    x = PReshape((10,10,1))(qinp) #10x10
+    x = HConv2D(5, (3,3), activation='relu', padding='valid', parallel_factor=64)(x) #5x8x8
+    x = PMaxPool2D((1,2))(x) #5x8x4
+    x = HActivation('relu')(x)
+    mp = PMaxPool2D((4,4))(x) #5x2x1
+    x = PReshape((10,))(mp) #5
+    x = HDense(10)(x)
+    y = PReshape((20,5))(qinp) #20x5
+    y = HConv1D(5, 5, strides=5, padding='valid', parallel_factor=4)(y) #4x5
+    po = PMaxPool1D(2)(y) #2x5
+    ay = HActivation('relu')(po)
+    y = PFlatten()(ay) #5
+    y = PDropout(0.5)(y)
+    y = HDense(10)(y)
+    z = PFlatten()(qinp) # 5
+    x = HAdd()([x,y]) # 5
+    xy = PConcatenate()([x,y]) # 5
+    z = HDense(10, activation='relu')(z) # 5
+    z = HDense(10, activation=None)(z) # 5
+    out = PConcatenate()([xy,z]) # 5
+    out = HActivation('relu')(out)
+    horrible_model = keras.Model(inp, out)
 
-    for layer in model.layers:
+    for layer in horrible_model.layers:
         # Randomize weight bitwidths
         if hasattr(layer, 'kernel_quantizer'):
             fbw:tf.Variable = layer.kernel_quantizer.fbw
-            fbw.assign(tf.constant(np.random.uniform(4,10,fbw.shape).astype(np.float32)))
+            fbw.assign(tf.constant(np.random.uniform(4,6,fbw.shape).astype(np.float32)))
         # And activation bitwidths
         if hasattr(layer, 'pre_activation_quantizer'):
             fbw:tf.Variable = layer.pre_activation_quantizer.fbw
             fbw.assign(tf.constant(np.random.uniform(2,6,fbw.shape).astype(np.float32)))
-    return model
+    return horrible_model
 
 def get_data(N:int, sigma:float, max_scale:float):
     rng = np.random.default_rng(42)
@@ -84,7 +97,7 @@ def test_end2end(N:int, rnd_strategy:str, io_type:str, cover_factor:float, aggre
     r_keras = model.predict(data, verbose=0) # type: ignore
     print(f"Testing {rnd_strategy}_{io_type}")
     proxy = generate_proxy_model(model, aggressive=aggressive)
-    output_dir=test_root_path / f'hls4ml_prj_hgq_{N}_{rnd_strategy}_{io_type}_overflow={cover_factor<1}_SAT={not aggressive}_{backend}'
+    output_dir=test_root_path / f'huge_hls4ml_prj_hgq_{N}_{rnd_strategy}_{io_type}_overflow={cover_factor<1}_SAT={not aggressive}_{backend}'
     model.save(output_dir / 'model.h5')
     proxy.save(output_dir / 'proxy.h5')
     r_proxy = proxy.predict(data, verbose=0) # type: ignore
