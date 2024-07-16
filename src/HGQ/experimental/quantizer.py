b61c57ecac826525f4b8a0c35837a5bfe5c07a91
Chang Sun <chsun@cern.ch>
2024-07-15 18:07:42 -0700

    ieee-like float

--- src/HGQ/experimental/quantizer.py
+++ src/HGQ/experimental/quantizer.py
@@ -5,19 +5,20 @@ import tensorflow as tf
 @tf.custom_gradient
 def floatlet_quantize(x, m, e, e0=0):
     '''Quantize an array to floatlet (m mantissa bits, e exponent bits) format. Tentative gradient impl.'''
-
+    # m+=1
     two = tf.constant(2.0, dtype=tf.float32)
     log2 = tf.math.log(two)
     eps = tf.constant(1e-30, dtype=tf.float32)
 
-    e_req = tf.math.floor(tf.math.log(tf.abs(x) + eps) / log2) + 1  # type: ignore
-    e_low, e_high = -two**(e - 1) + e0, two**(e - 1) - 1 + e0
-    e_act = tf.clip_by_value(e_req, e_low, e_high)
+    e_req = tf.math.floor(tf.math.log(tf.abs(x) + eps) / log2)  # type: ignore
+    _e_high = two**(e - 1)
+    e_low, e_high = -_e_high + e0, _e_high + e0 - 1
+    e_act = tf.minimum(e_req, e_high)
     scale = two**(e_act - m + 1)
     sig = x / scale
-    qsig = tf.floor(sig)
-    clip_sig = tf.clip_by_value(qsig, -two**(m - 1), two**(m - 1) - 1)
-    qx = clip_sig * scale
+    qsig = tf.floor(sig + 0.5)
+    clip_sig = tf.clip_by_value(qsig, -two**m + 1, two**m - 1)
+    qx = clip_sig * scale * tf.cast(e_act > e_low, tf.float32)
 
     def grad(dy: tf.Tensor):
         dm = scale * (sig - qsig) * dy * log2
@@ -34,8 +35,8 @@ def floatlet_decompose(x, m, e, e0=0):
     e_req = np.floor(np.log2(tf.abs(x) + eps)) + 1  # type: ignore
     e_low, e_high = -2.**e + e0, 2.**e - 1 + e0
     e_act = np.clip(e_req, e_low, e_high)
-    scale = 2.**(e_act - m + 1)
-    sig = x / scale
-    qsig = np.floor(sig)
-    clip_sig = np.clip(qsig, -2.**(m - 1), 2.**(m - 1) - 1) / 2**(m - 1)
+    scale = 2.**(e_act - m + 2)
+    sig = x / scale - 1
+    qsig = np.round(sig)
+    clip_sig = np.clip(qsig, -2.**m, 2.**m - 1) / 2**(m - 1)
     return clip_sig, e_act
